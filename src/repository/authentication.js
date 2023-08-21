272609b60a3cfe3a4be8c09e0c6ccd0e4bb83ef8
nikoladomazetovikj <nikolad150@gmail.com>
2023-08-21 23:15:10 +0200

    fix: token

--- src/repository/authentication.js
+++ src/repository/authentication.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { VueCookies } from 'vue-cookies';
 
 const apiClient = axios.create({
     baseURL: "http://127.0.0.1:8000",
@@ -7,6 +8,8 @@ export default {
     async registerUser (body) {
         try {
             const response = await apiClient.post("/register", body);
+            const token = response.data.token;
+            VueCookies.set('auth_token', token);
             return response; // Return the full response object
         } catch (error) {
             console.error('Failed to Register:', error);
@@ -15,12 +18,19 @@ export default {
     },
     async verificationLink(body) {
         try {
+            const token = VueCookies.get('auth_token');
+
+            if (!token) {
+                // Handle the case where the token is not present in the cookie
+                console.error('Token not found in cookie');
+                return;
+            }
             const response = await apiClient.post(
                 "/email/verification-notification",
                 { email: body.email }, // Sending email in the request body
                 {
                     headers: {
-                        Authorization: `Bearer ${body.token}` // Adding the bearer token to the headers
+                        Authorization: `Bearer ${token}` // Adding the bearer token to the headers
                     }
                 }
             );
