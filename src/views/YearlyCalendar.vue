8e361054aaed4ddacdd1b81e5585fa0a8c190462
LeoPavlovski <lp29379@seeu.edu.mk>
2023-08-21 22:53:23 +0200

    Fixed the date.

---
+++ src/views/YearlyCalendar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="yearly-calendar">
+    <MonthCalendar v-for="monthIndex in 12" :key="monthIndex" :monthIndex="monthIndex" :year="year" />
+  </div>
+</template>
+
+<script>
+import MonthCalendar from './MonthCalendar.vue'; // Path to your MonthCalendar component
+
+export default {
+  components: {
+    MonthCalendar,
+  },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.yearly-calendar {
+  display: flex;
+  grid-template-columns: repeat(3, 1fr); /* Adjust the number of columns as needed */
+  gap: 20px;
+}
+</style>
