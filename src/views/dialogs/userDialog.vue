f78917e5e15513b93022db7b0fa6760832c09619
nikolajovanoski <njovanoski2@gmail.com>
2023-09-08 18:16:10 +0200

    wip: User settings

--- src/views/dialogs/userDialog.vue
+++ src/views/dialogs/userDialog.vue
@@ -74,8 +74,8 @@
                 <template v-slot:activator="{ on, attrs }">
                   <v-text-field
                       v-model="date"
-                      label="Picker in menu"
-                      prepend-icon="mdi-calendar"
+                      label="Employment start date"
+                      append-icon="mdi-calendar"
                       readonly
                       v-bind="attrs"
                       v-on="on"
@@ -111,23 +111,60 @@
           <v-row>
             <!-- Date of Birth -->
             <v-col cols="6">
-              <v-text-field
-                  label="Date of Birth"
-                  v-model="user.dateOfBirth"
-                  outlined
-                  hide-details
-                  dense
-              ></v-text-field>
+              <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="date"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="date"
+                      label="Date of Birth"
+                      append-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                      outlined
+                      hide-details
+                      dense
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                    v-model="date"
+                    no-title
+                    scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      text
+                      color="primary"
+                      @click="menu = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.menu.save(date)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
             </v-col>
             <!-- Department -->
             <v-col cols="6">
-              <v-text-field
+              <v-select
                   label="Department"
                   v-model="user.department"
                   outlined
                   hide-details
                   dense
-              ></v-text-field>
+              ></v-select>
             </v-col>
           </v-row>
 
